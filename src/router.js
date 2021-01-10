74cd8615495f394c30e0d5847914c08611bd6a6a
Vincent Moittié <vincent.moittie@cji.paris>
2021-01-10 12:06:15 +0100

    Add historic of project's deployments view

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import ProjectDeploymentsHistoric from '@/views/ProjectDeploymentsHistoric.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/project/:projectName',
+      name: 'Historic of project\'s deployments',
+      component: ProjectDeploymentsHistoric,
     },
   ],
 });
