2d895477754b819a3cc14d017358de53505732e6
Vincent Moittié <vincent.moittie@cji.paris>
2021-07-25 11:51:07 +0200

    Create components to display entities

---
+++ src/components/DeploymentCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card
+    :class="deployment.color"
+    align="center"
+    justify="center">
+    <v-card-title>{{ deployment.client }}</v-card-title>
+    <v-card-text>
+      <template v-if="isNew">
+        <v-badge
+          top right
+          color="red accent-4"
+          class="text-h6">
+          <span
+            class="d-inline-block text-truncate"
+            style="max-width: 65px"
+            :title="deployment.version">
+            {{ deployment.version }}
+          </span>
+          <span
+            slot="badge"
+            :title="`Deployed less than ${isHotNew ? 'an hour ago' : 'a day ago'}.`"
+            style="cursor: help">
+            {{ isHotNew ? 'Hot' : 'New' }}
+          </span>
+        </v-badge>
+      </template>
+      <span
+        class="text-h6 d-inline-block text-truncate"
+        style="max-width: 75px"
+        :title="deployment.version"
+        v-else>
+        {{ deployment.version }}
+      </span>
+    </v-card-text>
+
+    <v-progress-linear
+      indeterminate
+      absolute
+      bottom
+      :active="deployment.inProgress"></v-progress-linear>
+  </v-card>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'DeploymentCard',
+  props: {
+    deployment: Object,
+  },
+  computed: {
+    insertDate() {
+      return moment.utc(this.deployment.insertDate);
+    },
+    isHotNew() {
+      const now = moment();
+      return this.isNew && (now.unix() - this.insertDate.unix()) <= 3600;
+    },
+    isNew() {
+      const now = moment();
+      return this.insertDate.year() === now.year()
+        && this.insertDate.month() === now.month()
+        && (now.unix() - this.insertDate.unix()) <= 86400;
+    },
+  },
+};
+</script>
---
+++ src/components/SubProjectsTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <v-breadcrumbs :items="links"></v-breadcrumbs>
+    <deployment-table :master-project="name"/>
+  </v-container>
+</template>
+
+<script>
+import DeploymentTable from '@/components/DeploymentTable.vue';
+
+export default {
+  name: 'SubProjectsTable',
+  components: { DeploymentTable },
+  data() {
+    const name = this.$route.params.projectName;
+    return {
+      name,
+      links: [{
+        text: 'Dashboard',
+        disabled: false,
+        href: '#/',
+      },
+      {
+        text: 'Master projects',
+        disabled: false,
+        href: '#/projects',
+      },
+      {
+        text: name,
+        disabled: true,
+        href: `#/projects/${name}`,
+      }],
+    };
+  },
+};
+</script>
