46a375bcc6e1cf91c8baa08989379e244112cfb6
Vincent Moittié <vincent.moittie@cji.paris>
2021-08-02 12:59:59 +0200

    Add mixin to manage authentication

---
+++ src/mixins/AuthenticationMixin.js
@@ -0,0 +1,58 @@
+const AuthenticationMixin = {
+  computed: {
+    isConnected() {
+      return this.$store.state.user.isConnected;
+    },
+    roles() {
+      return this.$store.state.user.roles;
+    },
+  },
+  methods: {
+    isUserGranted(roles) {
+      return roles.some((role) => this.roles.includes(role));
+    },
+    authenticate(storage, token, keepActive) {
+      return this.$http.get('/octo-spy/api/users/me', {
+        headers: {
+          Authorization: `Basic ${token}`,
+        },
+      }).then((response) => {
+        const data = {
+          token,
+          roles: response.data.roles,
+          ...response.data.user,
+        };
+        this.$store.commit('setUser', data);
+        if (keepActive) {
+          storage.setItem('user-token', token);
+        } else {
+          storage.removeItem('user-token');
+        }
+      }).catch((response) => {
+        this.$store.commit(
+          'showMessage',
+          { message: 'Wrong username or password.', color: 'error' },
+        );
+        return response;
+      });
+    },
+    authenticateFromStorage(storage) {
+      const token = storage.getItem('user-token');
+      if (token != null) {
+        this.authenticate(storage, token, true)
+          .catch(() => storage.removeItem('user-token'));
+      }
+    },
+    disconnect(storage) {
+      this.$store.commit('disconnect');
+      if (storage.getItem('user-token') != null) {
+        storage.removeItem('user-token');
+      }
+      if (this.$route.path !== '/') {
+        this.$router.push('/');
+      }
+    },
+  },
+};
+
+export default AuthenticationMixin;
