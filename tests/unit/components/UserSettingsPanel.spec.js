9214ccca01a5efb9aaf0a4cb578f1d86c4ee053d
Vincent Moittié <vincent.moittie@cji.paris>
2021-08-02 13:00:55 +0200

    Add authentication components

---
+++ tests/unit/components/UserSettingsPanel.spec.js
@@ -0,0 +1,47 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import component from '@/components/UserSettingsPanel.vue';
+import Vuetify from 'vuetify';
+
+const localVue = createLocalVue();
+
+describe('UserSettingsPanel', () => {
+  let wrapper;
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    wrapper = shallowMount(component, {
+      localVue,
+      vuetify,
+      mocks: {
+        $store: {
+          state: {
+            user: {
+              firstname: 'John',
+              lastname: 'Doe',
+              email: 'john.doe@mail.com',
+            },
+          },
+        },
+      },
+    });
+  });
+
+  it('Test computed: firstname', () => {
+    expect(wrapper.vm.firstname).toEqual('John');
+  });
+
+  it('Test computed: lastname', () => {
+    expect(wrapper.vm.lastname).toEqual('Doe');
+  });
+
+  it('Test computed: email', () => {
+    expect(wrapper.vm.email).toEqual('john.doe@mail.com');
+  });
+
+  it('Test method: exit', () => {
+    wrapper.vm.disconnect = jest.fn();
+    wrapper.vm.exit();
+    expect(wrapper.vm.disconnect).toBeCalled();
+  });
+});
---
+++ tests/unit/components/commons/Identicon.spec.js
@@ -0,0 +1,26 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import component from '@/components/commons/Identicon.vue';
+
+const localVue = createLocalVue();
+
+describe('Identicon', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(component, {
+      localVue,
+      propsData: {
+        value: 'admin',
+      },
+    });
+  });
+
+
+  it('Test default props', () => {
+    expect(wrapper.vm.size).toEqual(24)
+  });
+
+  it('Test computed: identicon', () => {
+    expect(wrapper.vm.identicon).not.toBeNull();
+  });
+});
